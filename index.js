502ad1bedba2b640dcd6e797cfab0d167567a187
Sandy Yeung <sandynism@gmail.com>
2017-09-12 00:56:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value })
-
 }
+  destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    
+  }
